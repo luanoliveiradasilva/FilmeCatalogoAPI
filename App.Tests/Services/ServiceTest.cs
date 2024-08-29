9fe89aa25053172c130261a7a3590c29bc330804
= <=>
2024-08-29 18:51:06 +0100

    adicionado test unit com covarage

--- App.Tests/Services/ServiceTest.cs
+++ App.Tests/Services/ServiceTest.cs
@@ -1,17 +1,41 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Xunit;
 using app.Services;
+using Moq;
+using app.Repository;
+using AutoFixture;
+using app.Models.DTO;
 
 namespace App.Test.Services;
 public class ServiceTest
 {
 
-    [Fact]
-    public void Test1()
+    private readonly IFilmesNetServices _filmesNetServices;
+    private readonly Mock<IFilmesRespository> _filmesRepositoryMock;
+    private readonly Fixture _fixture;
+
+    public ServiceTest()
     {
-        Assert.True(true);
+        _filmesRepositoryMock = new Mock<IFilmesRespository>();
+
+        _fixture = new Fixture();
+
+        _filmesNetServices = new FilmesNetServices(_filmesRepositoryMock.Object);
+    }
+
+    [Fact]
+    public void GetFilmes_ShouldReturnListOfFilmes()
+    {
+        // Arrange (Given)
+        var filmesDTOFake = _fixture.CreateMany<FilmesDTO>(5).ToList();
+
+        _filmesRepositoryMock
+        .Setup(repo => repo.GetFilmesAsync(It.IsAny<int>(), It.IsAny<int>()))
+        .ReturnsAsync(filmesDTOFake);
+
+        // Act (When)
+        var result = _filmesNetServices.GetFilmes(1, 5).Result;
+
+        // Assert (Then)
+        Assert.NotNull(result);
+        Assert.Equal(filmesDTOFake.Count, result.Count());
     }
 }
--- App.Tests/obj/Debug/net8.0/App.Test.AssemblyInfo.cs
+++ App.Tests/obj/Debug/net8.0/App.Test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9699714676e62dc6b0ae4aa81e7d0b9e4068d2fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7c19ddd19900f49fed11596d46aa6b92da2ed63")]
 [assembly: System.Reflection.AssemblyProductAttribute("App.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
