d7c19ddd19900f49fed11596d46aa6b92da2ed63
= <=>
2024-08-29 17:44:04 +0100

    adicionado teste unitario, ambos a csporj tem que estar abaixo da sln

---
+++ App.Tests/Services/ServiceTest.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using app.Services;
+
+namespace App.Test.Services;
+public class ServiceTest
+{
+
+    [Fact]
+    public void Test1()
+    {
+        Assert.True(true);
+    }
+}
